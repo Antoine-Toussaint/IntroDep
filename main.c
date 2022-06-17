497d748e6d2a075e0e8488ba817103622c2dca13
x0000000001 <antoine.toussaint@depinfonancy.fr>
2022-06-17 08:42:57 +0200

    initial commit

---
+++ main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include "module.h"
+
+int main()
+{
+    int a = fibonacci(5);
+    printf("%d\n",a);
+
+    return 0;
+}
---
+++ module.c
@@ -0,0 +1,10 @@
+#include "module.h"
+
+int addone(int n){
+    return n+1;
+}
+
+int fibonacci(int n)
+{
+    return addone(5);
+}
