d4a65937b1712f954aad9f47b44eb1c856ecacae
x0000000001 <antoine.toussaint@depinfonancy.fr>
2022-06-17 16:18:59 +0200

    nothing

--- TD2/main.c
+++ TD2/main.c
@@ -4,6 +4,7 @@
 
 int main()
 {
+    #pragma region tests
     // char s[] = "hey you";
     // char * s1 = copy_string(s);
     // printf("%s\n", s1);
@@ -14,36 +15,39 @@ int main()
     // char s[] = "hey you ebay";
     // printf("%s\n", modif_first_character(s));
 
-    int *t = empty();
-    printf("t initialized : ");
-    print_list(t);
+    // int *t = empty();
+    // printf("t initialized : ");
+    // print_list(t);
 
-    t = append(t, 5);
-    t = append(t, 78);
-    t = append(t, 15);
-    print_list(t);
+    // t = append(t, 5);
+    // t = append(t, 78);
+    // t = append(t, 15);
+    // print_list(t);
 
-    t = insert(t, 8);
-    t = insert(t, 9);
-    t = insert(t, 10);
-    print_list(t);
+    // t = insert(t, 8);
+    // t = insert(t, 9);
+    // t = insert(t, 10);
+    // print_list(t);
 
-    t = append(t, 4);
-    t = append(t, 5);
-    t = append(t, 6);
-    t = append(t, 7);
-    t = append(t, 8);
-    t = append(t, 9);
-    t = append(t, 10);
-    t = append(t, 11);
-    print_list(t);
+    // t = append(t, 4);
+    // t = append(t, 5);
+    // t = append(t, 6);
+    // t = append(t, 7);
+    // t = append(t, 8);
+    // t = append(t, 9);
+    // t = append(t, 10);
+    // t = append(t, 11);
+    // print_list(t);
 
-    t = insert(t, 7);
-    t = insert(t, 8);
-    t = insert(t, 9);
-    t = insert(t, 10);
-    t = insert(t, 11);
-    print_list(t);
+    // t = insert(t, 7);
+    // t = insert(t, 8);
+    // t = insert(t, 9);
+    // t = insert(t, 10);
+    // t = insert(t, 11);
+    // print_list(t);
+    #pragma endregion
+
+    
 
     return 0;
 }
